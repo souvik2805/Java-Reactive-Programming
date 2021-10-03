ea704acb3fcd49e6c29e6b84781febababb15aaf
souvik <souvik2805@gmail.com>
2021-10-03 16:42:47 +0530

    - Initial Setup

---
+++ src/main/java/reactivefromudemy/ReactiveFromUdemyApplication.java
@@ -0,0 +1,13 @@
+package reactivefromudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveFromUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveFromUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reactivefromudemy/ReactiveFromUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package reactivefromudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFromUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
